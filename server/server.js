1132e2d5be25025c2ca7d43ac69e41ee66cc1e43
Mike Fretwell <mfretwell@mGeo.ca>
2025-04-03 19:05:53 -0600

    Add frontPageRoutes to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+
+// Should be be doing away with CORS?
 import cors from 'cors';
 import superHeroRoutes from './routes/superHeroRoutes.js';
 import cityRoutes from './routes/cityRoutes.js';
+import frontPageRoutes from './routes/frontPageRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +14,8 @@ app.use(express.json());
 
 app.use('/superheroes', superHeroRoutes);
 app.use('/cities', cityRoutes);
+app.use('/cameras', frontPageRoutes);
+// app.use()
 
 const server = app.listen(PORT, () => {
     console.log('Server listening on port ' + PORT);
