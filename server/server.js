a470669802fdcaeb358620c96d2a714b881774ab
Mike Fretwell <mfretwell@mGeo.ca>
2025-04-03 19:28:56 -0600

    Comment frontPageRoutes within server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 
 import superHeroRoutes from './routes/superHeroRoutes.js';
 import cityRoutes from './routes/cityRoutes.js';
-import frontPageRoutes from './routes/frontPageRoutes.js';
+// import frontPageRoutes from './routes/frontPageRoutes.js';
 import cameraLocationRoutes from './routes/cameraLocations.js';
 
 // Load environment variables
@@ -20,7 +20,8 @@ app.use(express.json());
 
 app.use('/superheroes', superHeroRoutes);
 app.use('/cities', cityRoutes);
-app.use('/cameras', frontPageRoutes);
+
+// app.use('/cameras', frontPageRoutes);
 // app.use()
 app.use('/cameralocations', cameraLocationRoutes);
 
