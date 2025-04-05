ec6ac90f24131b5cce2068b9758041fb4bd81f8f
Mike Fretwell <mfretwell@mGeo.ca>
2025-04-05 14:55:12 -0600

    Remove shuffle test

--- server/models/ShuffleImage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const shuffleImageSchema = new mongoose.Schema({
-  url: String,
-  title: String,
-  description: String,
-});
-
-module.exports = mongoose.model('ShuffleImage', shuffleImageSchema);
