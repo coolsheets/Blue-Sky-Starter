9d4dac1bd4fc8a2a7af88614c10bb1cb4ba9f808
Robert <r.starak@hotmail.com>
2025-04-21 16:28:43 -0600

    Moving Tony's reaction card to separate modal

--- client/src/FrontPage.jsx
+++ client/src/FrontPage.jsx
@@ -1,34 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
-import "./FrontPage.css";
 import Footer from "./Footer";
 import VideoReactionCard from "./component/VideoReactionCard.jsx";
-
+import { Dialog, DialogContent } from "@mui/material";
+import "./FrontPage.css"; // for layout stuff
+import "./component/VideoReactionCard.css"; // for modal styling
 
 const FrontPage = () => {
-  const videoFiles = [
-    "003.mp4",
-    "006.mp4",
-    "009.mp4"
-  ];
+  const [open, setOpen] = useState(false);
+
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
+
+  const videoFiles = ["003.mp4", "006.mp4", "009.mp4"];
 
   return (
     <div className="front-page-container">
-      {/* Navbar */}
       <Navbar />
 
-      {/* Title Picture Section */}
       <header className="title-picture">
         <img src="/img/calgary2.jpg" alt="Scenic Calgary" className="title-picture-image" />
         <div className="title-picture-text">
           <p>Explore City timelaps</p>
           <h1>Choose <span> the best one</span></h1>
-          {/* <button>Vote</button> */}
-          <VideoReactionCard>Vote</VideoReactionCard>
+          <button onClick={handleOpenModal}>Vote</button>
         </div>
       </header>
 
-      {/* Video Thumbnails */}
       <section className="video-preview">
         {videoFiles.map((file, index) => (
           <div key={index} className="video-card">
@@ -36,7 +34,21 @@ const FrontPage = () => {
           </div>
         ))}
       </section>
+
       <Footer />
+
+      {/* Modal */}
+      <Dialog
+        open={open}
+        onClose={handleCloseModal}
+        maxWidth="md"
+        fullWidth
+        classes={{ backdrop: "custom-dialog-backdrop" }}
+      >
+        <DialogContent className="custom-dialog-content">
+          <VideoReactionCard videoUrl="/videos/003.mp4" />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
