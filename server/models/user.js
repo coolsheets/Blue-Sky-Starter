318984f2916d66ccc3223a793266b9fe66ac62cb
vathsalakh <vathsala.kh@gmail.com>
2025-04-23 23:08:44 -0600

    merge

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,15 @@
-import mongoose from "mongoose";
+import { connectDb } from "../db.js";
+// import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // In production, hash passwords!
-});
+const mongoose = await connectDb();
 
-export default mongoose.model("User", userSchema);
+// Schema 
+const UserSchema = new mongoose.Schema({
+    email:    { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  });
 
+// Models
+const User = mongoose.model("User", UserSchema);
 
-export async function createUser(username, password) {
-  const newUser = await User.create({ username, password });
-  return newUser;
-}
-export async function findUserByUsername(username) {
-  return await User.findOne({ username });
-}
+export default User;
